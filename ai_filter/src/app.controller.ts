6344aa2f0275a54a83b45caee9591e7347de0812
akira-erick <kajitani.motta@gmail.com>
2025-12-01 23:18:18 -0300

    feat(): adicionando algoritmos

--- ai_filter/src/app.controller.ts
+++ ai_filter/src/app.controller.ts
@@ -1,6 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//Expensive Hash Loop
+/*
+20k light
+50k medium
+100k+ heavy
+*/
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- ai_filter/src/app.service.ts
+++ ai_filter/src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { createHash } from 'crypto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  private expensiveHash(iterations: number): string {
+    let data = 'fake-image-data';
+
+    for (let i = 0; i < iterations; i++) {
+      data = createHash('sha256').update(data).digest('hex');
+    }
+
+    return data;
+  }
+
 }
